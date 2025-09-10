a8fc8b7f632537fec440f3a0f2674f0a62d7a632
RAKOTONIRINA Tarantsoa Corinah <142663825+rakotonirinatarantsoa@users.noreply.github.com>
2025-09-10 22:43:00 +0300

    createTable

---
+++ app/Models/evenementsModel.php
@@ -0,0 +1,14 @@
+<!-- syntaxe pour creer un evenement -->
+<!-- CREATE TABLE `evenements` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`nom` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`description` VARCHAR(50) NOT NULL COLLATE 'utf8mb4_general_ci',
+	`date` DATE NOT NULL,
+	`lieu` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`categories` VARCHAR(50) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`id`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+; -->
+
