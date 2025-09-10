1c2c2d0dde3224fc7e02e5649e81fb6891d83c75
Antsa <antsarakotonimanana65@gmail.com>
2025-09-10 15:51:58 +0300

    config de .env, ajout de page dans view, ajout de DB

---
+++ gestion Ticket/app/Views/accueil.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tickeko</title>
+</head>
+<body>
+    <style>
+        .test{
+            font-size: 48px;
+        }
+    </style>
+    <p class="test">Tongasoa</p>
+</body>
+</html>
