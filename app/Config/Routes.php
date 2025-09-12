1d6bd591c29ad6fe112871a5212bf85169094e78
Rico <randritonirico@gmail.com>
2025-09-12 08:40:31 +0300

    Rico

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::accueil');
+$routes->get('crud', 'Home::crudFanampiny');
+$routes->get('evenements', 'Home::eveneme');
+$routes->post('crud', 'Home::evenements');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\EventsModel;
+
 class Home extends BaseController
 {
     public function accueil(): string
     {
         return view('accueil');
     }
+
+    public function crudFanampiny(): string
+    {
+        return view('crud');
+    }
+
+    public function eveneme(): string
+    {
+        return view('evenements');
+    }
+
+
 }
