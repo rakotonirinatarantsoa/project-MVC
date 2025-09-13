604f9c32d29e916467cbd11de13d24ab34199027
RAKOTONIRINA Tarantsoa Corinah <142663825+rakotonirinatarantsoa@users.noreply.github.com>
2025-09-13 21:29:52 +0300

    modification slider et creation Page reserver

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,25 +5,17 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-// $routes->get('/', 'Home::accueil');
-// Chemin vers connexion
+
 $routes->get('/login', 'Home::login');
-// Chemin vers inscription
 $routes->get('/inscription', 'Home::inscription');
 $routes->get('crud', 'Home::crudFanampiny');
 $routes->get('evenements', 'Home::eveneme');
-$routes->post('crud', 'Home::evenements');
-
 $routes->get('/', 'EvenementController::accueil');
 $routes->post('create-evenement', 'EvenementController::createEvenement');
 $routes->get('/backoffice', 'EvenementController::afficherListeEvenement');
-
 $routes->get("evenements/modifierEvenement/(:num)", 'EvenementController::modifEv/$1');
-
 $routes->get('modifierEvenement', 'EvenementController::modifEv');
-
 $routes->get('evenements/modifier/(:num)', 'EvenementController::modifier/$1');
-
 $routes->get('evenements/supprimer/(:num)', 'EvenementController::supprimer/$1');
 
 // $routes->post('inscription', 'InscriptionController::FaireInscription');
--- app/Views/sections/slider-evenements.php
+++ app/Views/sections/slider-evenements.php
@@ -1,18 +1,24 @@
 <section id="events" class="events">
-    <div class="container-fluid" data-aos="fade-up">
+    <div class="container-fluid" data-aos="fa-up">
         <div class="section-header">
             <p>Tous <span>Vos Evènements</span></p>
         </div>
         <div class="slides-3 swiper" data-aos="fade-up" data-aos-delay="100">
             <div class="swiper-wrapper">
                 <?php foreach ($evenements as $evenement): ?>
-                    <div id="<?= $evenement['id'] ?>" class="swiper-slide event-item d-flex flex-column justify-content-end" style="background-image: url(assets/img/events-1.jpg)">
-                        <h3><?= $evenement['date'] ?></h3>
-                        <div class="price align-self-start"><?= $evenement['titre'] ?></div>
-                        <p class="description">
-                            <?= $evenement['description'] ?>
-                        </p>
-                    </div>
+                        <div id="<?= $evenement['id'] ?>" class="swiper-slide event-item d-flex flex-column justify-content-end" style="background-image: url(assets/img/<?= $evenement['image'] ?>)">
+                            <h3><?= $evenement['date'] ?></h3>
+                            <div class="price align-self-start"><?= $evenement['titre'] ?></div>
+                            <p class="description">
+                                <?= $evenement['description'] ?>
+                            </p>
+                            <p> <?= $evenement['lieu'] ?></p>
+                            <p> <?= $evenement['categorie'] ?></p>
+                            <p> <?= $evenement['places'] ?></p>
+                            <p> <?= $evenement['image'] ?></p>
+                    <a href="/reserver/<?= $evenement['id'] ?>">Reserver</a>
+            </div>
+
                 <?php endforeach; ?>
             </div>
             <div class="swiper-pagination"></div>
