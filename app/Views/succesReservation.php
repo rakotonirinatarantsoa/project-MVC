800a33278ff7baa3801ac20808b885392c777d8f
Antsa <antsarakotonimanana65@gmail.com>
2025-09-15 17:04:29 +0300

    finalisation 3.0

--- app/Views/succesReservation.php
+++ app/Views/succesReservation.php
@@ -32,8 +32,8 @@
 <body>
     <div class="bienvenue-text">
         <p class="bienvenue">Vous avez reservé <?= $nombre ?> billet(s) pour <?= $evenement['titre'] ?>  .<i class="bi bi-check-circle-fill" style="color: rgba(0, 218, 0, 1); font-size: 16px;"></i></p>
-        <p class="prix-total">Total <?= $montant?></p>
-        <a href="<?= site_url('/') ?>#main" class="connecter-btn" type="submit">Retour</a>
+        <p class="prix-total">Total <?= $montant?> Ar.</p>
+        <a href="<?= base_url('/accueilConnecte') ?>" class="connecter-btn">Retour</a>
     </div>
 
   <!-- Vendor JS Files -->
